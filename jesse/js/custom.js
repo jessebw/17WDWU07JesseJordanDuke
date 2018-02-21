2cb143a0a9c15843bd0aebe50753a0cb0ac81b0a
Jesse <jessebwalsh@gmail.com>
2018-02-21 16:15:04 +1300

    style

--- jesse/js/custom.js
+++ jesse/js/custom.js
@@ -1,3 +1,33 @@
-(function () { 
+/* iife */
+(function (){ 
 
-})();
+console.log('a');
+
+function getTwitter(){
+	$.ajax({
+		url:"js/MOCK_DATA.json",
+		dataType:"json",
+		success:function(dataFromServer){
+			console.log('success');
+
+		},
+		error:function(errorFromJSON){
+			console.log(errorFromJSON);
+			alert("Cannot connect to server.");
+		}
+	})
+}
+
+getTwitter();
+
+
+
+
+
+
+
+
+
+
+
+})(); /* /iife */
