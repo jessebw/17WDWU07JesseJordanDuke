06c429ca689385d8d7f02fe0c1229b991f5f9e6b
Jesse <jessebwalsh@gmail.com>
2018-03-06 12:03:53 +1300

    jesse

--- jesse/js/custom.js
+++ jesse/js/custom.js
@@ -1,33 +1,39 @@
-/* iife */
-(function (){ 
 
-// console.log('a');
+console.log('testA');
 
-// function getTwitter(){
-// 	$.ajax({
-// 		url:"js/MOCK_DATA.json",
-// 		dataType:"json",
-// 		success:function(dataFromServer){
-// 			console.log('success');
+function initAutocomplete(){
 
-// 		},
-// 		error:function(errorFromJSON){
-// 			console.log(errorFromJSON);
-// 			alert("Cannot connect to server.");
-// 		}
-// 	})
-// }
+    var input = document.getElementById('formInput');
 
-// getTwitter();
+    var acomplete = new google.maps.places.Autocomplete(input);
 
+    google.maps.event.addListener(acomplete, 'place_changed', function(){
+      
+        var place = acomplete.getPlace();
+        // console.log(place.formatted_address);
+        // console.log(place.url);
+        console.log(place);
 
+        var myLat = place.geometry.location.lat();
+        console.log(myLat);
 
+        var myLong = place.geometry.location.lng();
+        console.log(myLong);
 
+        var uluru = {lat: myLat, lng: myLong};
 
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: uluru
+        });
 
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+ 
+        
+    });
+   
+} /* /initAutocomplete */
 
-
-
-
-
-})(); /* /iife */
